972f305936dd0fa02207565c0750271f28c009d5
Mark Durrant <mark.j.durrant@gmail.com>
2017-06-03 14:24:46 +0100

    add map page

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,35 @@
-var positive = document.getElementById('btn-positive');
-var negative = document.getElementById('btn-negative');
+if (document.getElementById('mobile')) {
+  var positive = document.getElementById('btn-positive');
+  var negative = document.getElementById('btn-negative');
 
-var soundDir = "./assets/sounds/";
+  var soundDir = "./assets/sounds/";
 
-var bikeBell = new Audio(soundDir + 'bike-bellx3.wav');
-    bikeBell.loop = false;
+  var bikeBell = new Audio(soundDir + 'bike-bellx3.wav');
+      bikeBell.loop = false;
 
-var airHorn = new Audio(soundDir + 'air-horn.mp3');
-    airHorn.loop = false;
+  var airHorn = new Audio(soundDir + 'air-horn.mp3');
+      airHorn.loop = false;
 
-positive.onclick = function() {
-  console.log(':D');
+  positive.onclick = function() {
+    console.log(':D');
+    bikeBell.play();
+  };
 
-  bikeBell.play();
+  negative.onclick = function() {
+    console.log(':0');
+    airHorn.play();
+  };
 }
 
-negative.onclick = function() {
-  console.log(':D');
+// ====================================================
 
-  airHorn.play();
+if (document.getElementById('map-page')) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibWQtZnVydGhlcm1vcmUiLCJhIjoiY2ozZnAwcmoyMDAwZDMzcDlldnZ5OTc1MyJ9.uSeNdCs1ZiuXLO2e7ToJIg';
+
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-0.127758, 51.507351],
+    zoom: 13
+  });
 }
